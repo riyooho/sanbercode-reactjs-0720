e5323301ac165699fd920c3469a0387cf53ce618
staticriou <riyonugroho@outlook.com>
2020-07-30 12:26:23 +0700

    mengupload tugas hari ke-9

---
+++ Pekan 2/Tugas Hari ke-4/tugas9.js
@@ -0,0 +1,34 @@
+// soal 1
+const newFunction = (firstName, lastName) => {
+  return {
+    firstName,
+    lastName,
+    fullName() {
+      console.log(firstName, lastName)
+      return 
+    }
+  }
+}
+ 
+//Driver Code 
+newFunction("William", "Imoh").fullName()
+
+// soal 2
+const newObject = {
+  firstName: "Harry",
+  lastName: "Potter Holt",
+  destination: "Hogwarts React Conf",
+  occupation: "Deve-wizard Avocado",
+  spell: "Vimulus Renderus!!!"
+}
+const {firstName, lastName, destination, occupation} = newObject
+
+// Driver code
+console.log(firstName, lastName, destination, occupation)
+
+// soal 3
+const west = ["Will", "Chris", "Sam", "Holly"]
+const east = ["Gill", "Brian", "Noel", "Maggie"]
+const combined = [...west, ...east]
+//Driver Code
+console.log(combined)
